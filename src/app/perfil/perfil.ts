e059045b9d62036ddbbced83999935cfb0d9cdc5
gustavoasdolfo2 <gustavoasdolfo2@gmail.com>
2026-05-04 19:53:12 +0000

    Perfil HTML ADD

--- src/app/perfil/perfil.ts
+++ src/app/perfil/perfil.ts
@@ -3,7 +3,7 @@ import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
