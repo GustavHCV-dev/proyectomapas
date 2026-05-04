ca7277d864eefd531123a711ccd2c4a0a8ef7f4d
gustavoasdolfo2 <gustavoasdolfo2@gmail.com>
2026-05-04 22:47:09 +0000

    Terminar comunidad

--- src/app/comunidad/comunidad.ts
+++ src/app/comunidad/comunidad.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-comunidad',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './comunidad.html',
   styleUrl: './comunidad.css',
 })
